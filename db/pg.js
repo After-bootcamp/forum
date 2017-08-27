6c1adce5185df08d1543fccf6c6f2c76ba1a20c8
dlawr <dan.lawrence3@gmail.com>
2017-08-27 15:49:49 -0400

    exported pool, test connection in development

--- db/pg.js
+++ db/pg.js
@@ -1,5 +1,5 @@
 const env = require('../env.js');
-const { Pool, Client } = require('pg')
+const { Pool } = require('pg');
 
 const pool = new Pool({
   user: env.PGDB_USER,
@@ -7,23 +7,13 @@ const pool = new Pool({
   database: env.PGDB_NAME,
   password: env.PGDB_PASS,
   port: env.PGDB_PORT
-})
+});
 
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+if (!env.APP_ENV || env.APP_ENV === 'development') {
+  pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res);
+    pool.end();
+  });
+};
 
-const client = new Client({
-  user: env.PGDB_USER,
-  host: env.PGDB_HOST,
-  database: env.PGDB_NAME,
-  password: env.PGDB_PASS,
-  port: env.PGDB_PORT
-})
-client.connect()
-
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
+module.exports = pool;
