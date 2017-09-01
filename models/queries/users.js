f28b81a070fa475adab5704d755ccd63e944018c
dlawr <dan.lawrence3@gmail.com>
2017-08-31 22:11:12 -0400

    renamed db to models

---
+++ models/queries/users.js
@@ -0,0 +1,18 @@
+const pool = require('../pg');
+
+const user = {};
+
+user.getUserByEmail = (req, res, next) => {
+  try {
+    const data = await pool.query(`select * from users where email like $/email/`, req.body);
+    console.log('try');
+  } catch (e) {
+    console.error('getUserByEmail, users.js', e);
+  } finally {
+    console.log('finally');
+    res.data = data.rows;
+    next();
+  };
+};
+
+module.exports = user;
