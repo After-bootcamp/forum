5c527642c5084bf6f8b360e231865a3a892bfa18
dlawr <dan.lawrence3@gmail.com>
2017-09-01 21:25:47 -0400

    exported api routes to controller

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+var api = require('controllers/api.js');
 
 const app = express();
 const _port = process.argv[2] || process.env.PORT || 3005;
 
 app.use(bodyparser.json);
 app.use(express.static('public'));
+
+app.use('/api', api)
 
 app.listen(_port , console.log(`server listening on ${_port}`));
